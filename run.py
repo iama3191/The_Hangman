639932e2048a7fb0cc094c4384edc10a27b88385
iama3191 <ire.andreina.z@gmail.com>
2022-04-20 16:12:32 +0000

    add logic to check if the user's input is an alphabetic character and if it is a single character

--- run.py
+++ run.py
@@ -12,9 +12,9 @@ def intro():
     < ========================================== > \n
     Welcome to the Hangman Game!\n
     The rules are simple:\n
-    1. You need to guess a random word (HINT: The theme is countries of the world)\n
-    2. You have 8 tries until the hangman is completed\n
-    3. You can use only characters from the latin alphabet (vowels and consonants)\n
+    1. Guess a random word (HINT: The theme is countries of the world)\n
+    2. Use only 8 tries until the hangman is completed\n
+    3. Use only characters from the latin alphabet (vowels and consonants)\n
     < ========================================== > \n''')
     name = input('What is your name? ').capitalize()
     print(f'\nI\'m so glad that you are here {name}! Let\'s play and have fun!!\n')
@@ -23,7 +23,8 @@ def intro():
 
 def guess_word():
     """
-    Get a random word from the imported file words.py and return the word in uppercase
+    Get a random word from the imported file words.py and return 
+    the word in uppercase
     """
     return (random.choice(country_words)).upper()
 
@@ -42,7 +43,21 @@ def play_game():
     word_length = '_ ' * len(word)
     print('< ========================================== > \n')
     print(f'\t{word_length}\n')
-    print(player + '! your word has ' + str(len(word)) + ' letters')
-
+    print(player + '! your word has ' + str(len(word)) + ' letters\n')
+    while round < tries  and is_correct is False:
+        round += 1
+        print('\n+++++++++++++++++++++++++++++++++++')
+        print(f'Round: {round}\n')
+        player_guess = input('Enter any letter to check if it is in the secret word: ').upper()
+        if player_guess.isalpha() is True:
+            if len(player_guess) == 1:
+                if player_guess in word:
+                    print(f'You did it! {player_guess} is in the word')
+                else:
+                    incorrect_letters.append(player_guess)
+                print(f'Incorrect letters: {incorrect_letters}')
+        else:
+            print('Check your entry. Your input is not valid')
+        
 
 play_game()
