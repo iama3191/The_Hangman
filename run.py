0d2625515e8231a110b18e25df4642c5d103049f
iama3191 <ire.andreina.z@gmail.com>
2022-04-20 13:15:09 +0000

    Add intro function: it gives the welcome to the user, ask for his name and explain the rules

--- run.py
+++ run.py
@@ -3,4 +3,21 @@ from stages import hangman
 import random
 
 
+def intro():
+    """
+    Welcome the user to the Hangman Game, ask for the name, explain the 
+    rules and calls for the game function
+    """
+    print('''
+    < ========================================== > \n
+    Welcome to the Hangman Game!\n
+    The rules are simple:\n
+    1. You need to guess a random word (HINT: The theme is countries of the world)\n
+    2. You have 8 tries until the hangman is completed\n
+    3. You can use only characters from the latin alphabet (vowels and consonants)\n
+    < ========================================== > \n''')
+    name = input('What is your name? ').capitalize()
+    print(f'\nI\'m so glad that you are here {name}! Let\'s play and have fun!!')
+    start_game()
 
+intro()
